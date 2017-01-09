e199057ac137ad18edf8f930c5cb7226d342d2f1
Dario Cangialosi <dario.cangialosi@gmail.com>
2017-01-09 09:51:19 +0100

    added comments for the 3 phases (input, output, jump).

--- turing.c
+++ turing.c
@@ -84,9 +84,9 @@ int main(int argc, char* argv[]){
 	int c=0,x,i,dst,src;m[1]=1;int px=0,pdst=0;
 	while(1){
 		i=c<<2;dst=p.pa[i|0];src=p.pa[i|1];
-		x=src!=3?((src==0||src==1)?src:m[src]):scan_bit();
-		if(dst==3)print_bit(x);else if(dst!=0&&dst!=1)m[dst]=x;
-		c=p.pa[i|2|m[2]];
+		x=src!=3?((src==0||src==1)?src:m[src]):scan_bit(); // PHASE 1: INPUT
+		if(dst==3)print_bit(x);else if(dst!=0&&dst!=1)m[dst]=x; // PHASE 2: OUTPUT
+		c=p.pa[i|2|m[2]]; // PHASE 3: JUMP
 		if(src==4)break;//stop
 		if(dst==4){//program can be modified
 			for(int i=0;i<8;i++)set_bit(&px,m[5+0+i],i);
